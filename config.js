4379d03a49c32f37b02ddfe3e09b6e0c107eda56
hoangtien28 <95928108+hoangtien28@users.noreply.github.com>
2021-12-10 08:01:43 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb : "Tiêu đề web" ,
-    introTitle : 'Gửiem <3'
+    introTitle : 'Gửiem <3',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
